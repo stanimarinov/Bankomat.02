c9c0807338f62829eea8e28c6e737cf0c98efe19
Stani Marinov <stani.marinov@yahoo.com>
2023-11-13 17:21:39 +0200

    edit

--- docs/src/app.py
+++ docs/src/app.py
@@ -5,13 +5,14 @@ from bankomat.account import Account, Accounts
 
 if __name__=="__main__":
 
-    db = DB()
-
-    accounts_dict = db.get_all_accounts()
-
-    accounts = Accounts.from_list_of_dict(accounts_dict)
-    print(accounts)
-
-    accounts[0].balance = 0
-    for account in accounts:
-        print(account)
+        db = DB()
+    
+        accounts_dict = db.get_all_accounts()
+    
+        accounts = Accounts.from_list_of_dict(accounts_dict)
+        print(accounts)
+    
+        accounts[0].balance = 0
+        for account in accounts:
+                print(account)
+                
